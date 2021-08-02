0b6e46a8443b3be39c6fdcc98b7ec37e38fba111
Tony <tpg@blueyonder.co.uk>
2021-08-02 13:11:43 +0100

    Added Anion

--- src/Ion.hs
+++ src/Ion.hs
@@ -1,9 +1,18 @@
-module Ion where
+module Ion (
+  Ion(..),
+  loseElectrons,
+  gainElectrons
+) where
 
+import Electron
 import Proton
 import ChemicalElement
 
-data Ion = Cation { cationChemicalElement::ChemicalElement, protons::[Proton] } deriving Eq
+data Ion = Cation { cationChemicalElement::ChemicalElement, protons::[Proton] } |
+           Anion { anionChemicalElement::ChemicalElement, electrons::[Electron] } deriving Eq
 
-cation :: ChemicalElement -> Int -> Ion
-cation ce n = Cation { cationChemicalElement=ce, protons=replicate n proton }
+loseElectrons :: ChemicalElement -> Int -> Ion
+loseElectrons ce n = Cation { cationChemicalElement=ce, protons=replicate n proton }
+
+gainElectrons :: ChemicalElement -> Int -> Ion
+gainElectrons ce n = Anion { anionChemicalElement=ce, electrons=replicate n electron }
--- test/IonSpec.hs
+++ test/IonSpec.hs
@@ -4,14 +4,23 @@ import Test.Hspec
 
 import Ion
 import Proton
+import Electron
 import ChemicalElements
 
 spec :: Spec
 spec = do
   describe "Cation" $ do
-    it "Losing an electron" $ do
+    it "An atom losing an electron creates a cation" $ do
       let na = sodium
-      let x = cation na 1
+      let actual = na `loseElectrons` 1
 
-      cationChemicalElement x `shouldBe` na
-      protons x `shouldBe` [proton]
+      cationChemicalElement actual `shouldBe` na
+      protons actual `shouldBe` [proton]
+
+  describe "Anion" $ do
+    it "An atom gaining an electron creates an anion" $ do
+      let o = oxygen
+      let actual = o `gainElectrons` 2
+
+      anionChemicalElement actual `shouldBe` o
+      electrons actual `shouldBe` [electron, electron]
